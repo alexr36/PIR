2af6b3e40aac1645f28ebed821676bd7a2edf4e7
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-10-08 18:52:22 +0200

    Add files via upload

---
+++ List02/lab02_task02.ino
@@ -0,0 +1,83 @@
+// -----------------------------------------------------------------------------
+// Required definitions
+// -----------------------------------------------------------------------------
+#define LED_RED   6
+#define LED_GREEN 5
+#define LED_BLUE  3
+
+#define BUTTON_RED   2
+#define BUTTON_GREEN 4
+
+// -----------------------------------------------------------------------------
+// Global variables
+// -----------------------------------------------------------------------------
+uint16_t brightnessLevel = 127;   // 0 - OFF, 255 - full brightness
+
+// -----------------------------------------------------------------------------
+// Pin initialization
+// -----------------------------------------------------------------------------
+// Initialization of RGB pins
+void initRGB() {
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_RED, LOW);
+
+  pinMode(LED_GREEN, OUTPUT);
+  //digitalWrite(LED_GREEN, LOW);
+  analogWrite(LED_GREEN, brightnessLevel);
+
+  pinMode(LED_BLUE, OUTPUT);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+// Initialization of button pins
+void initButtons() {
+  pinMode(BUTTON_RED, INPUT_PULLUP);
+  pinMode(BUTTON_GREEN, INPUT_PULLUP);
+}
+
+// -----------------------------------------------------------------------------
+// Handling buttons and diode behavior
+// -----------------------------------------------------------------------------
+// Turning the brightness level down
+void handleButtonRed() {
+  if (digitalRead(BUTTON_RED) == LOW) {
+    if (brightnessLevel <= 0) return;
+    brightnessLevel--;
+    setDiodeBrightness();
+  }
+}
+
+// Turning the brightness level up
+void handleButtonGreen() {
+  if (digitalRead(BUTTON_GREEN) == LOW) {
+    if (brightnessLevel >= 255) return;
+    brightnessLevel++;
+    setDiodeBrightness();
+  }
+}
+
+// Setting the LED diode brightness level
+void setDiodeBrightness() {
+  analogWrite(LED_GREEN, brightnessLevel);
+}
+
+void handleDiodeBrightness() {
+  handleButtonRed();
+  handleButtonGreen();
+  delay(10);
+}
+
+// -----------------------------------------------------------------------------
+// Setup and main loop
+// -----------------------------------------------------------------------------
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  initRGB();
+  initButtons();
+}
+
+
+void loop() {
+  handleDiodeBrightness();
+}
